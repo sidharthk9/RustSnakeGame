15a8fc88e55da3e7776e2d2e962e2b566be30765
Sidharth K <sidharth195@gmail.com>
2020-12-30 10:19:02 +0400

    Silly Refactorings #1

--- src/draw.rs
+++ src/draw.rs
@@ -1,26 +1,31 @@
 use piston_window::{rectangle, Context, G2d};
 use piston_window::types::Color;
 
-const BLOCK_SIZE: f64 = 25.0;
 
-pub fn to_coord(game_coord: i32) -> f64 {
-    (game_coord as f64) * BLOCK_SIZE
+pub fn coordinate_conversion(value: i32, block_size: f64) -> f64 {
+    (value as f64) * block_size
 }
 
-pub fn to_coord_u32(game_coord: i32) -> u32 {
-    to_coord(game_coord) as u32
+//External Usage: main.rs
+pub fn u32_coordinate_conversion(value: i32, block_size: f64) -> u32 {
+    coordinate_conversion(value, block_size) as u32
 }
 
-pub fn draw_block(color: Color, x: i32, y: i32, con: &Context, g: &mut G2d) {
-    let gui_x = to_coord(x);
-    let gui_y = to_coord(y);
+pub fn draw_block(color: Color, x: i32, y: i32, context: &Context, gfx: &mut G2d) {
+    const BLOCK_SIZE: f64 = 25.0;
 
-    rectangle(color,[gui_x, gui_y, BLOCK_SIZE, BLOCK_SIZE], con.transform, g);
+    let gui_x = coordinate_conversion(x, BLOCK_SIZE);
+    let gui_y = coordinate_conversion(y, BLOCK_SIZE);
+
+
+    rectangle(color, [gui_x, gui_y, BLOCK_SIZE, BLOCK_SIZE], context.transform, gfx);
 }
 
-pub fn draw_rectangle( color: Color, x: i32, y: i32, width: i32, height: i32, con: &Context, g: &mut G2d) {
-    let x = to_coord(x);
-    let y = to_coord(y);
+pub fn draw_rectangle(color: Color, x: i32, y: i32, width: i32, height: i32, context: &Context, gfx: &mut G2d) {
+    const BLOCK_SIZE: f64 = 25.0;
 
-    rectangle(color,[x, y, BLOCK_SIZE * (width as f64), BLOCK_SIZE * (height as f64)], con.transform, g);
+    let x = coordinate_conversion(x, BLOCK_SIZE);
+    let y = coordinate_conversion(y, BLOCK_SIZE);
+
+    rectangle(color, [x, y, BLOCK_SIZE * (width as f64), BLOCK_SIZE * (height as f64)], context.transform, gfx);
 }
--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,33 @@ extern crate rand;
 use piston_window::*;
 use piston_window::types::Color;
 use crate::game::Game;
-use crate::draw::to_coord_u32;
+use crate::draw::u32_coordinate_conversion;
 
 mod draw;
 mod snake;
 mod game;
 
-const BACK_COLOR: Color = [0.5, 0.5, 0.5, 1.0];
-
 fn main() {
+    // [Red, Green, Blue, Alpha]
+    const BACKGROUND_COLOR: Color = [0.5, 0.5, 0.5, 1.0];
+    const BLOCK_SIZE: f64 = 25.0;
     let (width, height) = (20, 20);
 
-    let mut window: PistonWindow =
-        WindowSettings::new("Snake", [to_coord_u32(width), to_coord_u32(height)])
-            .exit_on_esc(true).build().unwrap();
+    let mut window: PistonWindow = WindowSettings::new("Snake.rs",
+                                                       [u32_coordinate_conversion(width, BLOCK_SIZE), u32_coordinate_conversion(height, BLOCK_SIZE)])
+        .exit_on_esc(true).build().unwrap();
 
     let mut game = Game::new(width, height);
+
     while let Some(event) = window.next() {
-        if let Some(Button::Keyboard(key)) = event.press_args() { game.key_pressed(key);}
-        window.draw_2d(&event, |c, g, _| {
-            clear(BACK_COLOR, g);
-            game.draw(&c, g);
-        });
+        if let Some(Button::Keyboard(key)) = event.press_args() { game.key_pressed(key); }
+
+        //Generates the game window
+        window.draw_2d(&event,
+                       |context, graphics, _| {
+                           clear(BACKGROUND_COLOR, graphics);
+                           game.draw(&context, graphics);
+                       });
 
         event.update(|arg| { game.update(arg.dt); });
     }
