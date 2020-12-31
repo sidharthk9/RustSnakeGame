784f0e3bd8f3ca62b977778ed1505917a7ebeaff
Sidharth K <sidharth195@gmail.com>
2020-12-31 15:55:34 +0400

    Silly Refactorings #3

--- src/game.rs
+++ src/game.rs
@@ -1,89 +1,96 @@
 use piston_window::*;
 use piston_window::types::Color;
 use rand::{thread_rng, Rng};
+
 use crate::snake::{Direction, Snake};
 use crate::draw::{draw_block, draw_rectangle};
 
-const FOOD_COLOR: Color = [0.80, 0.00, 0.00, 1.0];
-const BORDER_COLOR: Color = [0.00, 0.00, 0.00, 1.0];
-const GAMEOVER_COLOR: Color = [0.90, 0.00, 0.00, 0.5];
-const MOVING_PERIOD: f64 = 0.1;
-const RESTART_TIME: f64 = 1.0;
 
 pub struct Game {
     snake: Snake,
-    food_exists: bool,
+    food_state: bool,
     food_x: i32,
     food_y: i32,
     width: i32,
     height: i32,
-    game_over: bool,
+    fail_state: bool,
     waiting_time: f64,
 }
 
 impl Game {
+
     pub fn new(width: i32, height: i32) -> Game {
         Game {
             snake: Snake::new_snake(2, 2),
             waiting_time: 0.0,
-            food_exists: true,
-            food_x: 6,
-            food_y: 4,
+            food_state: true,
+            food_x: 5,
+            food_y: 5,
             width,
             height,
-            game_over: false
+            fail_state: false,
         }
     }
 
     pub fn key_pressed(&mut self, key: Key) {
-        if self.game_over { return; }
-        let dir = match key {
-            Key::Up     => Some(Direction::Up),
-            Key::Down   => Some(Direction::Down),
-            Key::Left   => Some(Direction::Left),
-            Key::Right  => Some(Direction::Right),
+        if self.fail_state { return; }
+
+        let dir_mapping = match key {
+            Key::Up => Some(Direction::Up),
+            Key::Down => Some(Direction::Down),
+            Key::Left => Some(Direction::Left),
+            Key::Right => Some(Direction::Right),
             _ => Some(self.snake.head_direction())
         };
 
-        if dir.unwrap() == self.snake.head_direction().opposite() { return; }
+        if dir_mapping.unwrap() == self.snake.head_direction().opposite() { return; }
 
-        self.update_snake(dir);
+        self.update_snake(dir_mapping);
     }
 
-    pub fn draw(&self, con: &Context, g: &mut G2d) {
-        self.snake.draw_body(con, g);
+    pub fn draw(&self, context: &Context, gfx: &mut G2d) {
+        // [Red, Green, Blue, Alpha]
+        const FOOD_COLOR: Color = [0.80, 0.00, 0.00, 1.0];
+        const BORDER_COLOR: Color = [0.00, 0.00, 0.00, 1.0];
+        const FAILURE_COLOR: Color = [0.90, 0.00, 0.00, 0.5];
 
-        if self.food_exists {
-            draw_block(FOOD_COLOR, self.food_x, self.food_y, con, g);
+        self.snake.draw_body(context, gfx);
+
+        if self.food_state {
+            draw_block(FOOD_COLOR, self.food_x, self.food_y, context, gfx);
         }
 
-        draw_rectangle(BORDER_COLOR, 0, 0, self.width, 1, con, g);
-        draw_rectangle(BORDER_COLOR, 0, self.height - 1, self.width, 1, con, g);
-        draw_rectangle(BORDER_COLOR, 0, 0, 1, self.height, con, g);
-        draw_rectangle(BORDER_COLOR, self.width - 1, 0, 1, self.height, con, g);
+        //Border consists of four conjoined rectangles. Heh.
+        draw_rectangle(BORDER_COLOR, 0, 0, self.width, 1, context, gfx);
+        draw_rectangle(BORDER_COLOR, 0, self.height - 1, self.width, 1, context, gfx);
+        draw_rectangle(BORDER_COLOR, 0, 0, 1, self.height, context, gfx);
+        draw_rectangle(BORDER_COLOR, self.width - 1, 0, 1, self.height, context, gfx);
 
-        if self.game_over {
-            draw_rectangle(GAMEOVER_COLOR, 0, 0, self.width, self.height, con, g);
+        if self.fail_state {
+            draw_rectangle(FAILURE_COLOR, 0, 0, self.width, self.height, context, gfx);
         }
     }
 
     pub fn update(&mut self, delta_time: f64) {
+        const MOVING_PERIOD: f64 = 0.1;
+        const RESTART_TIME: f64 = 1.0;
+
         self.waiting_time += delta_time;
 
-        if self.game_over {
+        if self.fail_state {
             if self.waiting_time > RESTART_TIME { self.restart(); }
             return;
         }
 
-        if !self.food_exists { self.add_food(); }
+        if !self.food_state { self.add_food(); }
 
         if self.waiting_time > MOVING_PERIOD { self.update_snake(None); }
     }
 
     fn check_eating(&mut self) {
         let (head_x, head_y): (i32, i32) = self.snake.head_position();
-        if self.food_exists && self.food_x == head_x && self.food_y == head_y {
-            self.food_exists = false;
+        if self.food_state && self.food_x == head_x && self.food_y == head_y {
+            self.food_state = false;
             self.snake.restore_tail();
         }
     }
@@ -94,6 +101,7 @@ impl Game {
         if self.snake.overlap_tail(next_x, next_y) {
             return false;
         }
+
         next_x > 0 && next_y > 0 && next_x < self.width - 1 && next_y < self.height - 1
     }
 
@@ -107,15 +115,14 @@ impl Game {
         }
         self.food_x = new_x;
         self.food_y = new_y;
-        self.food_exists = true;
+        self.food_state = true;
     }
 
     fn update_snake(&mut self, dir: Option<Direction>) {
         if self.check_if_snake_alive(dir) {
             self.snake.move_forward(dir);
             self.check_eating();
-        }
-        else { self.game_over = true; }
+        } else { self.fail_state = true; }
 
         self.waiting_time = 0.0;
     }
@@ -123,9 +130,9 @@ impl Game {
     fn restart(&mut self) {
         self.snake = Snake::new_snake(2, 2);
         self.waiting_time = 0.0;
-        self.food_exists = true;
+        self.food_state = true;
         self.food_x = 6;
         self.food_y = 4;
-        self.game_over = false;
+        self.fail_state = false;
     }
 }
